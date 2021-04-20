e97f8ab405696c78fbd6c2bab9b5bc5f649d9f21
ArantxaDR <arantxaderuiz@gmail.com>
2021-04-20 14:52:25 +0200

    working on router

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import "./styles/_App.scss";
 import { Route } from "react-router-dom";
 import Landing from "./components/Landing/Landing";
 import Aboutme from "./components/AboutMe/Aboutme";
-import Contact from "./components/Contact/Contact";
-import Projects from "./components/Projects/Projects";
+
 import Footer from "./components/Footer/Footer";
 
 function App() {
@@ -12,9 +11,6 @@ function App() {
       <main>
         <Route exact path="/" component={Landing} />
         <Route exact path="/aboutme" component={Aboutme} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/projects" component={Projects} />
-
         <Footer />
       </main>
     </>

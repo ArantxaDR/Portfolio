364abd052f05bfbf1ff63ec54586a3e7f1a35244
ArantxaDR <arantxaderuiz@gmail.com>
2021-04-22 10:52:00 +0200

    change router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import "./styles/_App.scss";
+//Router
 import { Route } from "react-router-dom";
+
+//Components
 import Landing from "./components/Landing/Landing";
 import Aboutme from "./components/AboutMe/Aboutme";
-
 import Footer from "./components/Footer/Footer";
+
+//Styles
+import "./styles/_App.scss";
 
 function App() {
   return (
     <>
       <main>
         <Route exact path="/" component={Landing} />
-        <Route exact path="/aboutme" component={Aboutme} />
+        <Route path="/aboutme" component={Aboutme} />
         <Footer />
       </main>
     </>

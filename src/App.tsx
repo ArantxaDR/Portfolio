04976af815ebdd9337641ca3f5b9505372ad8fcc
ArantxaDR <arantxaderuiz@gmail.com>
2021-04-22 11:14:43 +0200

    add router to contact & projects

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Route } from "react-router-dom";
 //Components
 import Landing from "./components/Landing/Landing";
 import Aboutme from "./components/AboutMe/Aboutme";
+import Contact from "./components/Contact/Contact";
+import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer/Footer";
 
 //Styles
@@ -15,6 +17,8 @@ function App() {
       <main>
         <Route exact path="/" component={Landing} />
         <Route path="/aboutme" component={Aboutme} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
         <Footer />
       </main>
     </>

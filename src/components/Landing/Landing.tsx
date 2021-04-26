fca6f1c76938954eac79b20be84961d728f51d28
ArantxaDR <arantxaderuiz@gmail.com>
2021-04-26 11:49:39 +0200

    change typewriter from landing

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,65 +1,17 @@
-//Hooks
-import { useEffect, useState } from "react";
-
 //Component
 import LandingButton from "../Buttons/landingBtn/LandingButton";
+import TypeWriter from "./TypeWriter";
 
 //style
 import "./_landing.scss";
 
-const phrases = [
-  "Hola, mi nombre es Arantxa.",
-  "Soy desarrolladora frontend Jr.",
-  "Bienvenida/o a mi portfolio."
-];
-
+const phrases = "Hola, mi nombre es Arantxa y soy desarrolladora frontend Jr.";
 export default function Landing() {
-  const [index, setIndex] = useState(0);
-  const [subIndex, setSubIndex] = useState(0);
-  const [reverse, setReverse] = useState(false);
-  const [blink, setBlink] = useState(true);
-
-  // typeWriter
-  useEffect(() => {
-    if (index === phrases.length) return;
-
-    if (
-      subIndex === phrases[index].length + 1 &&
-      index !== phrases.length - 1 &&
-      !reverse
-    ) {
-      setReverse(true);
-      return;
-    }
-
-    if (subIndex === 0 && reverse) {
-      setReverse(false);
-      setIndex((prev) => prev + 1);
-      return;
-    }
-
-    const timeout = setTimeout(() => {
-      setSubIndex((prev) => prev + (reverse ? -1 : 1));
-    }, Math.max(reverse ? 75 : subIndex === phrases[index].length ? 900 : 150, Math.random() * 350));
-
-    return () => clearTimeout(timeout);
-  }, [subIndex, index, reverse]);
-
-  // blinker
-  useEffect(() => {
-    const timeout2 = setTimeout(() => {
-      setBlink((prev) => !prev);
-    }, 300);
-    return () => clearTimeout(timeout2);
-  }, [blink]);
-
   return (
     <div className=" landing-wrapper">
       <div className="landing-wrapper__nes">
         <main className=" nes-balloon from-left">
-          <h2>{`${phrases[index].substring(0, subIndex)}${
-            blink ? "|" : ""
-          }`}</h2>
+          <TypeWriter phrases={phrases} />
         </main>
 
         <div className="btn">
---
+++ src/components/Landing/TypeWriter.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState, useRef } from "react";
+
+type TypeWriterProps = {
+  phrases: string;
+};
+export default function TypeWriter({ phrases }: TypeWriterProps) {
+  const index = useRef(0);
+
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    index.current = 0;
+    setText("");
+  }, []);
+
+  useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      setText((value) => value + phrases.charAt(index.current));
+      index.current += 1;
+    }, 100);
+    return () => {
+      clearTimeout(timeoutID);
+    };
+  }, [text, phrases]);
+
+  return <h1>{text}</h1>;
+}

c56681d487a335f6c6b531aa6a93759bed1c4c43
ArantxaDR <arantxaderuiz@gmail.com>
2021-04-29 11:32:48 +0200

    change cards css

--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -5,39 +5,45 @@ import "../../../../node_modules/nes.css/css/nes.css";
 export default function Project({ project }: { project: any }) {
   return (
     <div className="project-card">
-      <img
-        className="project-card__img"
-        src={project.preview}
-        alt={project.alt}
-      />
-      <h3 className="project-card__title nes-text is-error">{project.name}</h3>
-      <ul className="project-card__text scale">
-        <li>{project.description}</li>
-        <li>{project.description2}</li>
-        <li>{project.description3}</li>
-      </ul>
-      <ul className="project-card__list">
-        <li className="project-card__listItem">
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={project.webpage}
-            title="Demo">
-            <i className="nes-logo is-small" title="Enlace a la demo"></i>
-          </a>
-        </li>
-        <li className="project-container__listItem">
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={project.repository}
-            title="Repository">
-            <i
-              className="nes-octocat animate is-small"
-              title="Enlace al reporsitorio"></i>
-          </a>
-        </li>
-      </ul>
+      <figure className="card-figure">
+        <img
+          className="card-figure__img"
+          src={project.preview}
+          alt={project.alt}
+        />
+        <h3 className="card-figure__title nes-text is-error">{project.name}</h3>
+        <ul className="card-figure__layer__list">
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={project.webpage}
+              title="Demo">
+              <span
+                className="nes-logo is-small"
+                title="Enlace a la demo"></span>
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={project.repository}
+              title="Repository">
+              <span
+                className="nes-octocat animate is-small"
+                title="Enlace al reporsitorio"></span>
+            </a>
+          </li>
+        </ul>
+        <div className="card-figure__layer">
+          <ul className="card-figure__layer__text ">
+            <li>{project.description}</li>
+            <li>{project.description2}</li>
+            <li>{project.description3}</li>
+          </ul>
+        </div>
+      </figure>
     </div>
   );
 }

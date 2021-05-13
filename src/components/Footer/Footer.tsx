e92f953674467512577adc5b3f1c2afc58c976cd
ArantxaDR <arantxaderuiz@gmail.com>
2021-05-13 12:25:05 +0200

    add readme

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <small>
-        Made with
+        Made with&nbsp;
         <i className="nes-icon coin is-small"></i> by &copy; AratxaDR 2021
       </small>
     </footer>
--- src/components/Projects/Project/Project.tsx
+++ src/components/Projects/Project/Project.tsx
@@ -12,35 +12,36 @@ export default function Project({ project }: { project: any }) {
           alt={project.alt}
         />
         <h3 className="card-figure__title nes-text is-error">{project.name}</h3>
-        <ul className="card-figure__layer__list">
-          <li>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={project.webpage}
-              title="Demo">
-              <span
-                className="nes-logo is-small"
-                title="Enlace a la demo"></span>
-            </a>
-          </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={project.repository}
-              title="Repository">
-              <span
-                className="nes-octocat animate is-small"
-                title="Enlace al reporsitorio"></span>
-            </a>
-          </li>
+        <ul className="card-figure__layer__text ">
+          <li>{project.description}</li>
+          <li>{project.description2}</li>
+          <li>{project.description3}</li>
         </ul>
+
         <div className="card-figure__layer">
-          <ul className="card-figure__layer__text ">
-            <li>{project.description}</li>
-            <li>{project.description2}</li>
-            <li>{project.description3}</li>
+          <ul className="card-figure__layer__list">
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={project.webpage}
+                title="Demo">
+                <span
+                  className="nes-logo is-small"
+                  title="Enlace a la demo"></span>
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={project.repository}
+                title="Repository">
+                <span
+                  className="nes-octocat animate is-small"
+                  title="Enlace al reporsitorio"></span>
+              </a>
+            </li>
           </ul>
         </div>
       </figure>
